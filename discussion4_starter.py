55d56157f6a4bc566c652e85b2ee1eb5b7edc3c0
david burton <davidburtonct@MacBook-Pro-716.local>
2023-01-25 14:13:02 -0500

    adding invalid version

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -75,6 +75,12 @@ def main():
     print("Area:", r.area())
     print("Perimeter:", r.perimeter())
     print()
+    
+    r = Rectangle(-10, 10)
+    print(r)
+    print("Area:", r.area())
+    print("Perimeter:", r.perimeter())
+    print()
 
 if __name__ == "__main__":
     main()
