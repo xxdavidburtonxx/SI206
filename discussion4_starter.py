46fee9d57e5b94f71a5af67474a54fda9b9596d6
david burton <davidburtonct@MacBook-Pro-716.local>
2023-01-25 14:06:16 -0500

    Adding area

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -1,7 +1,10 @@
 class Rectangle():
     # Create the constructor "__init__" method
     # Arguments: width (an ingeter), height (an integer)
-    # 
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
     # It sets an instance variable, "width" to the passed argument, width
     # It sets an instance variable, "height" to the passed argument, height
 
@@ -15,7 +18,8 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
-
+    def __str__(self):
+        return "A rectangle with width " + str(self.width) + " and height " + str(self.height)
 
 
     # Create the "verify_input" method
@@ -26,25 +30,38 @@ class Rectangle():
 
     # YOUR CODE HERE
 
+    def verify_input(self):
+        if (self.width >= 0) & (self.height >= 0):
+            return True
+        else:
+            return False
+
+    # # Create the "area" method
+    # #
+    # # It first verifies inputs and return "Invalid input" if they are invalid.
+    # # Otherwise, it returns the area of the rectangle.
+
+    # # YOUR CODE HERE
+    # def area(self):
+    #     if self.verify_input() == False:
+    #         return "Invaluid input"
+    #     else:
+    #         return int(self.width) * int(self.height)
 
 
-    # Create the "area" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the area of the rectangle.
 
-    # YOUR CODE HERE
+    # # Create the "perimeter" method
+    # #
+    # # It first verifies inputs and return "Invalid input" if they are invalid.
+    # # Otherwise, it returns the perimeter of the rectangle.
 
+    # # YOUR CODE HERE
 
-
-    # Create the "perimeter" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the perimeter of the rectangle.
-
-    # YOUR CODE HERE
-    
-
+    # def perimeter(self):
+    #     if self.verify_input() == False:
+    #         return "Invaluid input"
+    #     else:
+    #         return 2*(int(self.width) + int(self.height))
 
 def main():
     r = Rectangle(10, 10)
